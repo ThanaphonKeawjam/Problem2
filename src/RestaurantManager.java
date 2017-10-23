3fb992fd4c686959dd580caf2004cf9ceef10026
Thanaphon Keawjam <32285825+ThanaphonKeawjam@users.noreply.github.com>
2017-10-23 19:54:09 +0700

    Update code

--- src/RestaurantManager.java
+++ src/RestaurantManager.java
@@ -45,13 +45,11 @@ public class RestaurantManager{
 	
 	public static String[] getMenuItems(){
 		loadMenu();
-	//	ArrayList<String> listMenu = isMenu;
 		String[] itemMenu = isMenu.toArray(new String[isMenu.size()]);
 		return itemMenu;
 	}
 	
 	public static double[] getPrices(){
-	//	ArrayList<Double> listPrice = isPrice;
 		double[] priceMenu = new double[isPrice.size()];
 		for (int x = 0; x< priceMenu.length; x++){
 			priceMenu[x] = isPrice.get(x);
